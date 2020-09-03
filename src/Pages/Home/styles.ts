239a8bb09931b2cbf855b310883c8fd09ae4cf89
BrunoPittaF <caiobrunopittaf@gmail.com>
2020-09-02 22:16:09 -0300

    commit inicial

---
+++ src/Pages/Home/styles.ts
@@ -0,0 +1,9 @@
+import styled  from "styled-components";
+import colors from '../../Assets/styles/colors.json';
+
+export const Container = styled.div`
+    h1{
+        color: ${colors.title};
+    }
+
+`
